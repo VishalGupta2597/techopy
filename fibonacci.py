de8dfe67a8a47bc126d77f34061d8a27d9707644
DELL <vishalg2597@gmail.com>
2019-06-25 23:20:23 +0530

    25_June Practice on while loop

--- fibonacci.py
+++ fibonacci.py
@@ -3,11 +3,21 @@ a = 0
 b = 1
 c = 0
 i = 1
-print(a)
+"""print(a)
 print(b)
 while i<=n:
     c = a + b
     a = b
     b = c
     print (c)
+    i = i + 1"""
+
+lst= [0,1]
+print("Fibonacci Series is :")
+while i<=n:
+    c = a + b
+    a = b
+    b = c
+    lst.append(c)
     i = i + 1
+print(lst)
