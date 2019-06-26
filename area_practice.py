7207df13bbd3b69ebd4ca888fdfc18951fd8785d
DELL <vishalg2597@gmail.com>
2019-06-26 08:47:02 +0530

    area of circle triangle

---
+++ area_practice.py
@@ -0,0 +1,70 @@
+from math import *
+           #Circle
+"""r=5
+area=3.14*pow(r,2)           # Area of circle
+circumference=2*3.14*r       #circumference of circle
+diameter=2*r                 #Diameter of circle
+
+print("The area of circle is : ",area)
+print("\n\nThe circumference of circle is : ",circumference)
+print("\n\nThe Diameter of circle is : ",diameter)"""
+
+
+        #Rectangle
+"""l=4                         
+b=5
+area = l*b                  # Area of Ractangle
+parameter = 2*(l+b)         # Parameter of Rectangle
+
+print("The area of Rectangle is : ",area)
+print("The parameter of Rectangle is : ",parameter)"""
+
+
+      #Triangle
+"""a=3
+b=4
+c=5
+s=(a+b+c)/2
+area=sqrt(s*(s-a)*(s-b)*(s-c))
+
+print("The area of Triangle is : ",area)"""
+
+
+    #Simple Interest and Compound Interest
+"""p=16000    #Principle Amount
+r=5     #rate of interest
+t=2      #time
+
+si= (p+r+t)/100            #Simple Interest
+ci=  p*pow((1+r/100),t)    #compound Interest
+
+print("Simple Interest is : ",si)
+print("Compound Interest is : ",ci)"""
+
+
+      #Centigrade to Fahrenheit
+"""C=50
+F=C*9/5+32
+print("Centigrade to Fahrenheit : ",F)"""
+
+
+     #Fahrenheit to Centigrade
+"""F=122
+C=(F-32)*5/9
+print("Fahrenheit to Centigrade : ",C)"""
+
+
+      # Average
+Hin=45
+Eng=43
+Math=49
+Phy=48
+Che=44
+total=Hin+Eng+Math+Phy+Che
+
+Avg=total/5
+
+print("Sum of total subject no. is : ",total)
+print("Average is : ",Avg)
+
+
